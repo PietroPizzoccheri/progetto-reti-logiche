338e44a2c473090c0dc9d14655fde74ababa018d
Pietro <pietro.pizzoccheri@mail.polimi.it>
2024-04-24 09:53:36 +0200

    edge_cases_handler modified

--- EDGE_CASES_HANDLER.vhd
+++ EDGE_CASES_HANDLER.vhd
@@ -40,11 +40,11 @@ architecture RTL of EDGE_CASES_HANDLER is
   signal mantix_X, mantix_Y : std_logic_vector(22 downto 0);
   signal f                  : std_logic_vector(2 downto 0); -- function of the truth table 
   signal X_case, Y_case     : std_logic_vector(4 downto 0); -- truth table cases
-  constant NaN      : std_logic_vector(4 downto 0) := "00001";
-  constant INFINITY : std_logic_vector(4 downto 0) := "00010";
-  constant ZERO_CONST    : std_logic_vector(4 downto 0) := "00100";
-  constant DENORM   : std_logic_vector(4 downto 0) := "01000";
-  constant NORM     : std_logic_vector(4 downto 0) := "10000";
+  --constant NaN      : std_logic_vector(4 downto 0) := "00001";
+  --constant INFINITY : std_logic_vector(4 downto 0) := "00010";
+  --constant CONST    : std_logic_vector(4 downto 0) := "00100";
+  --constant DENORM   : std_logic_vector(4 downto 0) := "01000";
+  --constant NORM     : std_logic_vector(4 downto 0) := "10000";
 
   signal zero_X, zero_Y, inf_X, inf_Y, nan_X, nan_Y, norm_X, norm_Y, denorm_X, denorm_Y : std_logic;
 
@@ -83,45 +83,39 @@ begin
               inf    => inf_Y);
 
   -- check for edge case combinations
-  -- zero    <= (zero_X and norm_Y) or (norm_X and zero_Y) or (zero_X and denorm_Y) or (denorm_X and zero_Y) or (zero_X and zero_Y);
-  -- invalid <= (nan_X and norm_Y) or (norm_X and nan_Y) or (nan_X and denorm_Y) or (denorm_X and nan_Y) or (nan_X and zero_Y) or (zero_X and nan_Y) or (nan_X and inf_Y) or (inf_x and nan_Y) or (zero_X and inf_Y) or (inf_X and zero_Y) or (nan_X and nan_Y);
-  -- inf     <= (inf_X and norm_Y) or (norm_X and inf_Y) or (inf_X and denorm_Y) or (denorm_X and inf_Y) or (inf_X and inf_Y) or (inf_Y and inf_X);
-  X_case <= norm_X & denorm_X & zero_X & inf_X & nan_X; -- truth table for X
-  Y_case <= norm_Y & denorm_X & zero_Y & inf_Y & nan_Y; -- truth table for Y
-
+  zero    <= (zero_X and norm_Y) or (norm_X and zero_Y) or (zero_X and denorm_Y) or (denorm_X and zero_Y) or (zero_X and zero_Y);
+  invalid <= (nan_X and norm_Y) or (norm_X and nan_Y) or (nan_X and denorm_Y) or (denorm_X and nan_Y) or (nan_X and zero_Y) or (zero_X and nan_Y) or (nan_X and inf_Y) or (inf_x and nan_Y) or (zero_X and inf_Y) or (inf_X and zero_Y) or (nan_X and nan_Y);
+  inf     <= (inf_X and norm_Y) or (norm_X and inf_Y) or (inf_X and denorm_Y) or (denorm_X and inf_Y) or (inf_X and inf_Y) or (inf_Y and inf_X);
+  --X_case <= norm_X & denorm_X & zero_X & inf_X & nan_X; -- truth table for X
+  --Y_case <= norm_Y & denorm_X & zero_Y & inf_Y & nan_Y; -- truth table for Y
   -- truth table 
-  f <= "001" when X_case = NaN and Y_case = NaN else
-       "001" when X_case = NaN and Y_case = INFINITY else
-       "001" when X_case = NaN and Y_case = ZERO_CONST else
-       "001" when X_case = NaN and Y_case = DENORM else
-       "001" when X_case = NaN and Y_case = NORM else
-
-       "001" when X_case = INFINITY and Y_case = NaN else
-       "010" when X_case = INFINITY and Y_case = INFINITY else
-       "001" when X_case = INFINITY and Y_case = ZERO_CONST else
-       "010" when X_case = INFINITY and Y_case = DENORM else
-       "010" when X_case = INFINITY and Y_case = NORM else
-
-       "001" when X_case = ZERO_CONST and Y_case = NaN else
-       "001" when X_case = ZERO_CONST and Y_case = INFINITY else
-       "100" when X_case = ZERO_CONST and Y_case = ZERO_CONST else
-       "100" when X_case = ZERO_CONST and Y_case = DENORM else
-       "100" when X_case = ZERO_CONST and Y_case = NORM else
-
-       "001" when X_case = DENORM and Y_case = NaN else
-       "010" when X_case = DENORM and Y_case = INFINITY else
-       "100" when X_case = DENORM and Y_case = ZERO_CONST else
-       "000" when X_case = DENORM and Y_case = DENORM else
-       "000" when X_case = DENORM and Y_case = NORM else
-
-       "001" when X_case = NORM and Y_case = NaN else
-       "010" when X_case = NORM and Y_case = INFINITY else
-       "100" when X_case = NORM and Y_case = ZERO_CONST else
-       "000" when X_case = NORM and Y_case = DENORM else
-       "000" when X_case = NORM and Y_case = NORM;
-
-  invalid <= f(0);
-  inf     <= f(1);
-  zero    <= f(2);
-
+  --f <=  "001" when X_case = NaN and Y_case = NaN else
+  --      "001" when X_case = NaN and Y_case = INFINITY else
+  --      "001" when X_case = NaN and Y_case = CONST else
+  --      "001" when X_case = NaN and Y_case = DENORM else
+  --      "001" when X_case = NaN and Y_case = NORM else
+  --      "001" when X_case = INFINITY and Y_case = NaN else
+  --      "010" when X_case = INFINITY and Y_case = INFINITY else
+  --      "001" when X_case = INFINITY and Y_case = CONST else
+  --      "010" when X_case = INFINITY and Y_case = DENORM else
+  --      "010" when X_case = INFINITY and Y_case = NORM else
+  --      "001" when X_case = CONST and Y_case = NaN else
+  --      "001" when X_case = CONST and Y_case = INFINITY else
+  --      "100" when X_case = CONST and Y_case = CONST else
+  --      "100" when X_case = CONST and Y_case = DENORM else
+  --      "100" when X_case = CONST and Y_case = NORM else
+  --      "001" when X_case = DENORM and Y_case = NaN else
+  --      "010" when X_case = DENORM and Y_case = INFINITY else
+  --      "100" when X_case = DENORM and Y_case = CONST else
+  --      "000" when X_case = DENORM and Y_case = DENORM else
+  --      "000" when X_case = DENORM and Y_case = NORM else
+  --      "001" when X_case = NORM and Y_case = NaN else
+  --      "010" when X_case = NORM and Y_case = INFINITY else
+  --      "100" when X_case = NORM and Y_case = CONST else
+  --      "000" when X_case = NORM and Y_case = DENORM else
+  --       "000" when X_case = NORM and Y_case = NORM;
+  --invalid <= f(0);
+  --inf     <= f(1);
+  --zero    <= f(2);
 end architecture;
+
