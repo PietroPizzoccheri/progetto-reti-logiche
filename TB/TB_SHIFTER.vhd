599e49bd1b37f948a59d4da443ee8ca8914c6c2a
Luca Zani <lucazani2002@gmail.com>
2024-04-28 13:28:35 +0200

    Fixato TB_SHIFTER

--- TB/TB_SHIFTER.vhd
+++ TB/TB_SHIFTER.vhd
@@ -1,6 +1,10 @@
 library ieee;
   use ieee.std_logic_1164.all;
 
+  -- Uncomment the following library declaration if using
+  -- arithmetic functions with Signed or Unsigned values
+  --USE ieee.numeric_std.ALL;
+
 entity TB_SHIFTER is
 end entity;
 
@@ -9,49 +13,75 @@ architecture behavior of TB_SHIFTER is
   -- Component Declaration for the Unit Under Test (UUT)
   component SHIFTER
     port (
-      MANTIX  : in  STD_LOGIC_VECTOR(22 downto 0);
-      SHIFTED : out STD_LOGIC_VECTOR(22 downto 0)
+      CLK     : in  std_logic;
+      MANTIX  : in  std_logic_vector(22 downto 0);
+      SHIFTED : out std_logic_vector(22 downto 0)
     );
   end component;
 
   --Inputs
-  signal MANTIX : std_logic_vector(22 downto 0);
+  signal CLK    : std_logic                     := '0';
+  signal MANTIX : std_logic_vector(22 downto 0) := (others => '0');
 
   --Outputs
   signal SHIFTED : std_logic_vector(22 downto 0);
-  -- No clocks detected in port list. Replace <clock> below with 
-  -- appropriate port name 
+
+  -- Clock period definitions
+  constant CLK_period : time := 10 ns;
 
 begin
 
   -- Instantiate the Unit Under Test (UUT)
   uut: SHIFTER
     port map (
+      CLK     => CLK,
       MANTIX  => MANTIX,
       SHIFTED => SHIFTED
     );
 
-  -- Stimulus process
-  process
+  -- Clock process definitions
+
+  CLK_process: process
   begin
-    MANTIX <= "00000000000000000000000";
-    wait for 20 ns;
+    CLK <= '0';
+    wait for CLK_period / 2;
+    CLK <= '1';
+    wait for CLK_period / 2;
+  end process;
 
-    MANTIX <= "00000000011100000000000";
-    wait for 20 ns;
+  -- Stimulus process
 
-    MANTIX <= "00000000111111000000000";
-    wait for 20 ns;
+  stim_proc: process
+  begin
+    -- hold reset state for 100 ns.
+    wait for 100 ns;
 
-    MANTIX <= "00111110000000000000000";
-    wait for 20 ns;
+    -- Test case 1
+    MANTIX <= "00000000000000000000001";
+    wait for CLK_period;
+    assert SHIFTED = "00000000000000000000010" report "Test case 1 failed" severity error;
 
-    MANTIX <= "00000000000000000011100";
-    wait for 20 ns;
+    -- Test case 2: Input is '00000000000000000000010'
+    MANTIX <= "00000000000000000000010";
+    wait for CLK_period;
+    assert SHIFTED = "00000000000000000000100" report "Test case 2 failed" severity error;
 
-    MANTIX <= "11111111111100000000000";
+    -- Test case 3
+    MANTIX <= "00000000000000000000100";
+    wait for CLK_period;
+    assert SHIFTED = "00000000000000000001000" report "Test case 3 failed" severity error;
+
+    -- Test case 4
+    MANTIX <= "00000000000000000001000";
+    wait for CLK_period;
+    assert SHIFTED = "00000000000000000010000" report "Test case 4 failed" severity error;
+
+    -- Test case 5
+    MANTIX <= "00000000000000000010000";
+    wait for CLK_period;
+    assert SHIFTED = "00000000000000000100000" report "Test case 5 failed" severity error;
+
     wait;
-
   end process;
 
 end architecture;
