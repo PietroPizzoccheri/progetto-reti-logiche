25a39061898c08081d287f4ea5cb321032d5c475
Luca Zani <lucazani2002@gmail.com>
2024-05-01 12:10:42 +0200

    Fixed Prodotti Parziali

--- MUL/CLA_MUL_23.vhd
+++ MUL/CLA_MUL_23.vhd
@@ -19,28 +19,29 @@ architecture rtl of CLA_MUL_23 is
     );
   end component;
 
-  signal TEMP_X : std_logic_vector(45 downto 0);
   type T_t is array (0 to 23) of std_logic_vector(45 downto 0);
-  signal T : T_t;
+
+  signal TEMP_X : T_t;
+  signal TEMP_Y : T_t;
+  signal T      : T_t;
 begin
-  TEMP_X <= (22 downto 0 => '0') & X;
+  TEMP_X(0) <= (22 downto 0 => '0') & X;
 
   gen_pp: for i in 0 to 22 generate
-    TEMP_X <= TEMP_X(44 downto 0) & '0';
-    T(i)   <= (45 downto 0 => Y(i)) and TEMP_X;
+    TEMP_Y(i)     <= (45 downto 0 => Y(i));
+    TEMP_X(i + 1) <= TEMP_X(i)(44 downto 0) & '0';
+    T(i)          <= TEMP_Y(i) and TEMP_X(i);
   end generate;
 
-  gen_cla: for i in 0 to 22 generate
-    CLA_46_i: CLA_46
-      port map (
-        X    => T(i),
-        Y    => T(i + 1),
-        Cin  => '0',
-        S    => T(i + 1),
-        Cout => open
-      );
-  end generate;
-
-  P <= T(23);
-
+  --   gen_cla: for i in 0 to 22 generate
+  --     CLA_46_i: CLA_46
+  --       port map (
+  --         X    => T(i),
+  --         Y    => T(i + 1),
+  --         Cin  => '0',
+  --         S    => T(i + 1),
+  --         Cout => open
+  --       );
+  --   end generate;
+  --   P <= T(23);
 end architecture;
