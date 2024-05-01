b7308f227ee065299f0b46d246930df613f2c605
Pietro <pietro.pizzoccheri@mail.polimi.it>
2024-05-01 13:39:28 +0200

    working behavioural MULT

--- MUL/CLA_MUL_23.vhd
+++ MUL/CLA_MUL_23.vhd
@@ -21,11 +21,16 @@ architecture rtl of CLA_MUL_23 is
 
   type T_t is array (0 to 23) of std_logic_vector(45 downto 0);
 
-  signal TEMP_X : T_t;
-  signal TEMP_Y : T_t;
-  signal T      : T_t;
+  signal TEMP_X      : T_t;
+  signal TEMP_Y      : T_t;
+  signal T           : T_t;
+  signal PARTIAL_SUM : T_t;
 begin
   TEMP_X(0) <= (22 downto 0 => '0') & X;
+  PARTIAL_SUM(0) <= T(1);
+  TEMP_X(23) <= (45 downto 0 => '0');
+  TEMP_Y(23) <= (45 downto 0 => '0');
+
 
   gen_pp: for i in 0 to 22 generate
     TEMP_Y(i)     <= (45 downto 0 => Y(i));
@@ -33,15 +38,24 @@ begin
     T(i)          <= TEMP_Y(i) and TEMP_X(i);
   end generate;
 
-  --   gen_cla: for i in 0 to 22 generate
-  --     CLA_46_i: CLA_46
-  --       port map (
-  --         X    => T(i),
-  --         Y    => T(i + 1),
-  --         Cin  => '0',
-  --         S    => T(i + 1),
-  --         Cout => open
-  --       );
-  --   end generate;
-  --   P <= T(23);
+  CLA_46_i: CLA_46
+    port map (
+      X    => T(0),
+      Y    => PARTIAL_SUM(0),
+      Cin  => '0',
+      S    => PARTIAL_SUM(1),
+      Cout => open
+    );
+  gen_cla: for i in 1 to 22 generate
+    CLA_46_i: CLA_46
+      port map (
+        X    => T(i),
+        Y    => PARTIAL_SUM(i),
+        Cin  => '0',
+        S    => PARTIAL_SUM(i + 1),
+        Cout => open
+      );
+  end generate;
+
+  P <= PARTIAL_SUM(23);
 end architecture;
