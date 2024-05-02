92151554954bfbbf501655f1af18bc12c4ead567
Luca Zani <lucazani2002@gmail.com>
2024-05-02 18:54:50 +0200

    Fixed bug with OFFSET_ENCODER
    removed don't care since they don't work in the VHDL version Xilinx Support

--- TB/TB_OFFSET_ENCODER.vhd
+++ TB/TB_OFFSET_ENCODER.vhd
@@ -9,14 +9,14 @@ architecture behavior of TB_OFFSET_ENCODER is
   -- Component Declaration for the Unit Under Test (UUT)
   component OFFSET_ENCODER
     port (
-      X : in  std_logic_vector(22 downto 0);
+      X : in  std_logic_vector(23 downto 0);
       Y : out std_logic_vector(4 downto 0);
       Z : out std_logic
     );
   end component;
 
   --Inputs
-  signal X : std_logic_vector(22 downto 0) := (others => '0');
+  signal X : std_logic_vector(23 downto 0) := (others => '0');
 
   --Outputs
   signal Y : std_logic_vector(4 downto 0);
@@ -39,36 +39,36 @@ begin
   stim_proc: process
   begin
     -- Edge Case
-    X <= "00000000000000000000000";
+    X <= "000000000000000000000000";
     EXPECTED_Y <= "-----";
     EXPECTED_Z <= '1';
     wait for 50 ns;
     assert (Y = EXPECTED_Y) report "Edge Case failed" severity error;
     assert (Z = EXPECTED_Z) report "Edge Case failed" severity error;
 
-    X <= "10000000000000000000000";
+    X <= "100000000000000000000000";
     EXPECTED_Y <= "00001";
     EXPECTED_Z <= '0';
     wait for 50 ns;
     assert (Y = EXPECTED_Y) report "Test 1 failed" severity error;
     assert (Z = EXPECTED_Z) report "Test 1 failed" severity error;
 
-    X <= "00000000000000000000001";
-    EXPECTED_Y <= "10111";
+    X <= "000000000000000000000001";
+    EXPECTED_Y <= "11000";
     EXPECTED_Z <= '0';
     wait for 50 ns;
     assert (Y = EXPECTED_Y) report "Test 2 failed" severity error;
     assert (Z = EXPECTED_Z) report "Test 2 failed" severity error;
 
-    X <= "00000000000100000000000";
-    EXPECTED_Y <= "01100";
+    X <= "000000000000100000000000";
+    EXPECTED_Y <= "01101";
     EXPECTED_Z <= '0';
     wait for 50 ns;
     assert (Y = EXPECTED_Y) report "Test 3 failed" severity error;
     assert (Z = EXPECTED_Z) report "Test 3 failed" severity error;
 
-    X <= "00000000000100001010000";
-    EXPECTED_Y <= "01100";
+    X <= "000000000000100001010000";
+    EXPECTED_Y <= "01101";
     EXPECTED_Z <= '0';
     wait for 50 ns;
     assert (Y = EXPECTED_Y) report "Test 4 failed" severity error;
