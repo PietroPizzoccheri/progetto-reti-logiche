d67649355727b1372876a278846125f47b3085a6
Pietro <pietro.pizzoccheri@mail.polimi.it>
2024-05-03 10:10:55 +0200

    working multiplier 24 bit

--- CLA_MUL_23.vhd
+++ CLA_MUL_23.vhd
@@ -3,41 +3,41 @@ library IEEE;
 
 entity CLA_MUL_23 is
   port (
-    X : in  std_logic_vector(22 downto 0);
-    Y : in  std_logic_vector(22 downto 0);
-    P : out std_logic_vector(45 downto 0)
+    X : in  std_logic_vector(23 downto 0);
+    Y : in  std_logic_vector(23 downto 0);
+    P : out std_logic_vector(47 downto 0)
   );
 end entity;
 
+
 architecture rtl of CLA_MUL_23 is
-  component CLA_46 is
+  component CLA_48 is
     port (
-      X, Y : in  std_logic_vector(46 - 1 downto 0);
+      X, Y : in  std_logic_vector(47 downto 0);
       Cin  : in  std_logic;
-      S    : out std_logic_vector(46 - 1 downto 0);
+      S    : out std_logic_vector(47 downto 0);
       Cout : out std_logic
     );
   end component;
 
-  type T_t is array (0 to 23) of std_logic_vector(45 downto 0);
+  type T_t is array (0 to 23) of std_logic_vector(47 downto 0);
+  type T_temp_x is array (0 to 24) of std_logic_vector(47 downto 0);
 
-  signal TEMP_X      : T_t;
+  signal TEMP_X      : T_temp_x;
   signal TEMP_Y      : T_t;
   signal T           : T_t;
-  signal PARTIAL_SUM : T_t;
+  signal PARTIAL_SUM : T_temp_x;
 begin
-  TEMP_X(0)      <= (22 downto 0 => '0') & X;
+  TEMP_X(0)      <= (23 downto 0 => '0') & X;
   PARTIAL_SUM(0) <= T(1);
-  TEMP_X(23)     <= (45 downto 0 => '0');
-  TEMP_Y(23)     <= (45 downto 0 => '0');
 
-  gen_pp: for i in 0 to 22 generate
-    TEMP_Y(i)     <= (45 downto 0 => Y(i));
-    TEMP_X(i + 1) <= TEMP_X(i)(44 downto 0) & '0';
+  gen_pp: for i in 0 to 23 generate
+    TEMP_Y(i)     <= (47 downto 0 => Y(i));
+    TEMP_X(i + 1) <= TEMP_X(i)(46 downto 0) & '0';
     T(i)          <= TEMP_Y(i) and TEMP_X(i);
   end generate;
 
-  CLA_46_0: CLA_46
+  CLA_48_0: CLA_48
     port map (
       X    => T(0),
       Y    => T(1),
@@ -46,8 +46,8 @@ begin
       Cout => open
     );
 
-  gen_cla: for i in 2 to 22 generate
-    CLA_46_i: CLA_46
+  gen_cla: for i in 2 to 23 generate
+    CLA_48_i: CLA_48
       port map (
         X    => T(i),
         Y    => PARTIAL_SUM(i),
@@ -57,5 +57,5 @@ begin
       );
   end generate;
 
-  P <= PARTIAL_SUM(22);
+  P <= PARTIAL_SUM(24);
 end architecture;
--- TB/TB_CLA_MUL_23.vhd
+++ TB/TB_CLA_MUL_23.vhd
@@ -13,19 +13,19 @@ architecture behavior of TB_CLA_MUL_23 is
   -- Component Declaration for the Unit Under Test (UUT)
   component CLA_MUL_23
     port (
-      X : in  std_logic_vector(22 downto 0);
-      Y : in  std_logic_vector(22 downto 0);
-      P : out std_logic_vector(45 downto 0)
+      X : in  std_logic_vector(23 downto 0);
+      Y : in  std_logic_vector(23 downto 0);
+      P : out std_logic_vector(47 downto 0)
     );
   end component;
 
   --Inputs
-  signal X : std_logic_vector(22 downto 0) := (others => '0');
-  signal Y : std_logic_vector(22 downto 0) := (others => '0');
+  signal X : std_logic_vector(23 downto 0) := (others => '0');
+  signal Y : std_logic_vector(23 downto 0) := (others => '0');
 
   --Outputs
-  signal P          : std_logic_vector(45 downto 0);
-  signal EXPECTED_P : std_logic_vector(45 downto 0);
+  signal P          : std_logic_vector(47 downto 0);
+  signal EXPECTED_P : std_logic_vector(47 downto 0);
 begin
 
   -- Instantiate the Unit Under Test (UUT)
@@ -40,21 +40,22 @@ begin
 
   stim_proc: process
   begin
-    X <= "00000000000000000000010";
-    Y <= "00000000000000000000010";
-    EXPECTED_P <= "0000000000000000000000000000000000000000000100";
+    X <= "000000000000000000000010";
+    Y <= "000000000000000000000010";
+    EXPECTED_P <= "000000000000000000000000000000000000000000000100";
     wait for 50 ns;
     assert (P = EXPECTED_P) report "Test 1 failed" severity error;
 
-    X <= "11111111111000000000000";
-    Y <= "11111111111000000000000";
-    EXPECTED_P <= "0111111111100000000000100000000000000000000000";
+    X <= "000000000000000000000111";
+    Y <= "000000000000000000000111";
+    EXPECTED_P <= "000000000000000000000000000000000000000000110001";
     wait for 50 ns;
     assert (P = EXPECTED_P) report "Test 2 failed" severity error;
 
-    X <= "00000000000000000000111";
-    Y <= "00000000000000000000111";
-    EXPECTED_P <= "0000000000000000000000000000000000000000110001";
+    
+	 X <= "111111111110000000000000";
+    Y <= "111111111110000000000000";
+    EXPECTED_P <= "011111111110000000000010000000000000000000000000";
     wait for 50 ns;
     assert (P = EXPECTED_P) report "Test 3 failed" severity error;
 
