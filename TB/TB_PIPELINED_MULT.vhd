48aafb4c449bf3a1849d68a48cb23318e35efd09
Pietro <pietro.pizzoccheri@mail.polimi.it>
2024-05-09 13:39:04 +0200

    timed simulation working

--- TB/TB_PIPELINED_MULT.vhd
+++ TB/TB_PIPELINED_MULT.vhd
@@ -26,12 +26,12 @@ architecture behavior of TB_PIPELINED_MULT is
   signal P              : STD_LOGIC_VECTOR(31 downto 0);
   signal invalid_output : STD_LOGIC;
 
-  -- Utility signals
-  signal expected_output  : STD_LOGIC_VECTOR(31 downto 0);
-  signal expected_invalid : STD_LOGIC;
+  --utils 
+  signal expected_output         : STD_LOGIC_VECTOR(31 downto 0);
+  signal expected_invalid_output : STD_LOGIC;
 
   -- Clock period definitions
-  constant CLK_period : time := 60 ns;
+  constant CLK_period : time := 70 ns;
 begin
 
   -- Instantiate the Unit Under Test (UUT)
@@ -61,53 +61,53 @@ begin
   begin
     -- hold reset state for 100 ns.
     RST <= '1';
-    wait for 100 ns;
+    wait for CLK_period * 3;
     RST <= '0';
+    wait for CLK_period / 2;
 
     -- Test 1
     input_X <= "00111111100000000000000000000000";
     input_Y <= "00111111100000000000000000000000";
-
-    expected_output <= "00111111100000000000000000000000";
-    expected_invalid <= '0';
     wait for CLK_period;
-    assert (P = expected_output) and (invalid_output = expected_invalid) report "Test 1 failed" severity error;
 
     -- Test 2
     input_X <= "11000000000001100110011001100110";
     input_Y <= "01000000101111001100110011001101";
-
-    expected_output <= "11000001010001100011110101110000";
-    expected_invalid <= '0';
     wait for CLK_period;
-    assert (P = expected_output) and (invalid_output = expected_invalid) report "Test 2 failed" severity error;
 
     -- Test 3
     input_X <= "11110001111111110000000011111111";
     input_Y <= "01001011010000111110000100000011";
-
-    expected_output <= "11111101110000110001110111100101";
-    expected_invalid <= '0';
     wait for CLK_period;
-    assert (P = expected_output) and (invalid_output = expected_invalid) report "Test 3 failed" severity error;
+    expected_output <= "00111111100000000000000000000000";
+    expected_invalid_output <= '0';
+    assert (P = expected_output) and (invalid_output = expected_invalid_output) report "Test 1 failed" severity error;
 
     -- Test 4
     input_X <= "10000000000001110001110011111111";
     input_Y <= "01111111011111111110000100000011";
-
-    expected_output <= "10111110011000111000010001010010";
-    expected_invalid <= '0';
     wait for CLK_period;
-    assert (P = expected_output) and (invalid_output = expected_invalid) report "Test 4 failed" severity error;
+    expected_output <= "11000001010001100011110101110000";
+    expected_invalid_output <= '0';
+    assert (P = expected_output) and (invalid_output = expected_invalid_output) report "Test 2 failed" severity error;
 
     -- Test 5
     input_X <= "11111111100000000000000000000000";
     input_Y <= "01101111011110000000000100000011";
-
-    expected_output <= "11111111100000000000000000000000";
-    expected_invalid <= '0';
     wait for CLK_period;
-    assert (P = expected_output) and (invalid_output = expected_invalid) report "Test 5 failed" severity error;
+    expected_output <= "11111101110000110001110111100101";
+    expected_invalid_output <= '0';
+    assert (P = expected_output) and (invalid_output = expected_invalid_output) report "Test 3 failed" severity error;
+
+    wait for CLK_period;
+    expected_output <= "10111110011000111000010001010010";
+    expected_invalid_output <= '0';
+    assert (P = expected_output) and (invalid_output = expected_invalid_output) report "Test 4 failed" severity error;
+
+    wait for CLK_period;
+    expected_output <= "11111111100000000000000000000000";
+    expected_invalid_output <= '0';
+    assert (P = expected_output) and (invalid_output = expected_invalid_output) report "Test 5 failed" severity error;
 
     wait;
   end process;
