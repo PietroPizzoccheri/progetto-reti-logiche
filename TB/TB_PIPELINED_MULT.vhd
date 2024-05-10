728117e24b35d556d87d92bc34ed859b33ede741
Luca Zani <lucazani2002@gmail.com>
2024-05-10 21:04:31 +0200

    Edge case testing

--- TB/TB_PIPELINED_MULT.vhd
+++ TB/TB_PIPELINED_MULT.vhd
@@ -26,9 +26,20 @@ architecture behavior of TB_PIPELINED_MULT is
   signal P              : STD_LOGIC_VECTOR(31 downto 0);
   signal invalid_output : STD_LOGIC;
 
-  --utils 
+  -- Utils 
   signal expected_output         : STD_LOGIC_VECTOR(31 downto 0);
   signal expected_invalid_output : STD_LOGIC;
+
+  constant One            : STD_LOGIC_VECTOR(31 downto 0) := "00111111100000000000000000000000";
+  constant LargestDenorm  : STD_LOGIC_VECTOR(31 downto 0) := "00000000011111111111111111111111";
+  constant LargestNorm    : STD_LOGIC_VECTOR(31 downto 0) := "01111111011111111111111111111111";
+  constant SmallestDenorm : STD_LOGIC_VECTOR(31 downto 0) := "00000000000000000000000000000001";
+  constant SmallestNorm   : STD_LOGIC_VECTOR(31 downto 0) := "00000000100000000000000000000000";
+
+  constant Zero             : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+  constant NegativeInfinity : STD_LOGIC_VECTOR(31 downto 0) := "11111111100000000000000000000000";
+  constant PositiveInfinity : STD_LOGIC_VECTOR(31 downto 0) := "01111111100000000000000000000000";
+  constant NotANumber       : STD_LOGIC_VECTOR(31 downto 0) := "11111111110000001000010000000000";
 
   -- Clock period definitions
   constant CLK_period : time := 70 ns;
@@ -44,7 +55,6 @@ begin
       P              => P,
       invalid_output => invalid_output
     );
-
 
   -- Clock process definitions
 
@@ -66,6 +76,95 @@ begin
     RST <= '0';
     wait for CLK_period / 2;
 
+    -- Edge cases tests
+    -- Zero results: we should get 0 as a result
+    expected_output <= Zero;
+    expected_invalid_output <= '0';
+
+    -- 0 * 0 = 0
+    input_X <= Zero;
+    input_Y <= Zero;
+    wait for CLK_period;
+
+    -- 0 * Norm = 0
+    input_X <= Zero;
+    input_Y <= LargestNorm;
+    wait for CLK_period;
+
+    -- 0 * Denorm = 0
+    input_X <= Zero;
+    input_Y <= LargestDenorm;
+    wait for CLK_period;
+
+    assert (P = expected_output) and (invalid_output = expected_invalid_output) severity error;
+    wait for CLK_period;
+    assert (P = expected_output) and (invalid_output = expected_invalid_output) severity error;
+    wait for CLK_period;
+    assert (P = expected_output) and (invalid_output = expected_invalid_output) severity error;
+
+    -- Infinity results: we should get Infinity as a result
+    expected_output <= PositiveInfinity;
+    expected_invalid_output <= '0';
+
+    -- Infinity * Infinity = +Infinity
+    input_X <= PositiveInfinity;
+    input_Y <= PositiveInfinity;
+    wait for CLK_period;
+
+    -- Infinity * Norm = Infinity
+    input_X <= PositiveInfinity;
+    input_Y <= LargestNorm;
+    wait for CLK_period;
+
+    -- Infinity * Denorm = Infinity
+    input_X <= PositiveInfinity;
+    input_Y <= LargestDenorm;
+    wait for CLK_period;
+
+    assert (P = expected_output) and (invalid_output = expected_invalid_output) severity error;
+    wait for CLK_period;
+    assert (P = expected_output) and (invalid_output = expected_invalid_output) severity error;
+    wait for CLK_period;
+    assert (P = expected_output) and (invalid_output = expected_invalid_output) severity error;
+
+    -- Invalid results: we should get invalid as a result since the multiplication is not possible
+    expected_output <= (others => '-');
+    expected_invalid_output <= '1';
+
+    -- NaN * NaN = NaN
+    input_X <= NotANumber;
+    input_Y <= NotANumber;
+    wait for CLK_period;
+
+    -- NaN * Norm = NaN
+    input_X <= NotANumber;
+    input_Y <= LargestNorm;
+    wait for CLK_period;
+
+    -- NaN * Denorm = NaN
+    input_X <= NotANumber;
+    input_Y <= LargestDenorm;
+    wait for CLK_period;
+    assert (invalid_output = expected_invalid_output) severity error;
+
+    -- NaN * 0 = NaN
+    input_X <= NotANumber;
+    input_Y <= Zero;
+    wait for CLK_period;
+    assert (invalid_output = expected_invalid_output) severity error;
+
+    -- NaN * Infinity = NaN
+    input_X <= NotANumber;
+    input_Y <= PositiveInfinity;
+    wait for CLK_period;
+
+    assert (invalid_output = expected_invalid_output) severity error;
+    wait for CLK_period;
+    assert (invalid_output = expected_invalid_output) severity error;
+    wait for CLK_period;
+    assert (invalid_output = expected_invalid_output) severity error;
+
+    -- TODO: Write more meaningful tests
     -- Test 1
     input_X <= "00111111100000000000000000000000";
     input_Y <= "00111111100000000000000000000000";
@@ -100,38 +199,13 @@ begin
     expected_invalid_output <= '0';
     assert (P = expected_output) and (invalid_output = expected_invalid_output) report "Test 3 failed" severity error;
 
-    -- Test 6
-    input_X <= "11111111111111111111111111111111";
-    input_Y <= "00001100000001111111111100000000";
     wait for CLK_period;
-    expected_output <= "10111110011000111000010001010010";
-    expected_invalid_output <= '0';
-    assert (P = expected_output) and (invalid_output = expected_invalid_output) report "Test 4 failed" severity error;
 
-    -- Test 7
-    input_X <= "";
-    input_Y <= "";
     wait for CLK_period;
     expected_output <= "11111111100000000000000000000000";
     expected_invalid_output <= '0';
     assert (P = expected_output) and (invalid_output = expected_invalid_output) report "Test 5 failed" severity error;
 
-    -- Test 8
-    input_X <= "";
-    input_Y <= "";
-    wait for CLK_period;
-    expected_output <= "11111111111111111111111111111111";
-    expected_invalid_output <= '0';
-    assert (P = expected_output) and (invalid_output = expected_invalid_output) report "Test 6 failed" severity error;
-
-    -- Test 9
-    input_X <= "10000000011111111111100001111111";
-    input_Y <= "00000000000001111111111101100000";
-    wait for CLK_period;
-    expected_output <= "";
-    expected_invalid_output <= '0';
-    assert (P = expected_output) and (invalid_output = expected_invalid_output) report "Test 7 failed" severity error;
-    
     wait;
   end process;
 
